b6af26845acbab0deb4e03bae63a72bf3c0f3a5d
Robert Oleynik <robert.oleynik@proton.me>
2022-09-13 17:32:05 +0200

    Enable logging for advanced_services example.

--- examples/advanced_services.rs
+++ examples/advanced_services.rs
@@ -1,32 +1,36 @@
 use micro_tower::prelude::*;
 use micro_tower::runtime::Runtime;
 use micro_tower::{codegen::service, service::Service};
+use serde::{Deserialize, Serialize};
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
-// #[derive(Deserialize, Serialize)]
-// pub struct Request {}
+#[derive(Deserialize, Serialize)]
+pub struct Request {}
 
-// #[derive(Deserialize, Serialize)]
-// pub struct Response {
-//     message: String,
-// }
-
-// #[service(buffer = 64)]
-// async fn buffered_service(req: Request) -> Response {
-//     Response {
-//         message: String::from("Hello, World!"),
-//     }
-// }
+#[derive(Deserialize, Serialize)]
+pub struct Response {
+    message: String,
+}
 
 #[service(buffer = 64)]
-async fn buffered_service(req: i32) -> i32 {
-    req
+async fn buffered_service(_req: Request) -> Response {
+    Response {
+        message: String::from("Hello, World!"),
+    }
 }
 
 #[tokio::main]
 async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed setting default logging subscriber.");
+
     let buffered_service = Service::<buffered_service>::builder().build().unwrap();
 
-    let rt = Runtime::default()
+    Runtime::default()
         .bind_service(8080, buffered_service)
         .run()
         .await;
