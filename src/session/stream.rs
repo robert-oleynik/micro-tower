8ef4d785b5cec08f5b00bc393af59143839b1fc0
Robert Oleynik <robert.oleynik@proton.me>
2022-09-22 17:34:36 +0200

    Refactor stream session spawning (#20)

--- src/session/stream.rs
+++ src/session/stream.rs
@@ -10,46 +10,42 @@ use crate::util::BoxError;
 
 const BUF_SIZE: usize = 1024;
 
-pub struct Task {}
-
-impl Task {
-    pub fn spawn<St, Sv>(
-        mut stream: St,
-        mut service: Sv,
-    ) -> impl Future<Output = Result<(), BoxError>>
-    where
-        St: AsyncReadExt + AsyncWriteExt + Unpin + Send + 'static,
-        Sv: tower::Service<BytesMut, Response = BytesMut, Error = api::Error> + Send + 'static,
-        Sv::Future: Send,
-    {
-        async move {
-            let mut buf = BytesMut::new();
-            let mut lbuf = [0_u8; BUF_SIZE];
+pub fn spawn_fut<St, Sv>(
+    mut stream: St,
+    mut service: Sv,
+) -> impl Future<Output = Result<(), BoxError>>
+where
+    St: AsyncReadExt + AsyncWriteExt + Unpin + Send + 'static,
+    Sv: tower::Service<BytesMut, Response = BytesMut, Error = api::Error> + Send + 'static,
+    Sv::Future: Send,
+{
+    async move {
+        let mut buf = BytesMut::new();
+        let mut lbuf = [0_u8; BUF_SIZE];
+        loop {
+            buf.clear();
             loop {
-                buf.clear();
-                loop {
-                    let num = stream.read(&mut lbuf).await.map_err(Box::new)?;
-                    buf.extend_from_slice(&lbuf[..num]);
-                    if num < BUF_SIZE {
-                        break;
-                    }
+                let num = stream.read(&mut lbuf).await.map_err(Box::new)?;
+                buf.extend_from_slice(&lbuf[..num]);
+                if num < BUF_SIZE {
+                    break;
                 }
-                let ready = match service.ready().await {
-                    Ok(service) => service,
-                    Err(err) => {
-                        return Err(err.err);
-                    }
-                };
-                buf = match ready.call(buf).await {
-                    Ok(buf) => buf,
-                    Err(err) => {
-                        let report = Report::new(err.err.as_ref()).pretty(true);
-                        tracing::error!("{report:?}");
-                        err.buf
-                    }
-                };
-                stream.write_buf(&mut buf).await.map_err(Box::new)?;
             }
+            let ready = match service.ready().await {
+                Ok(service) => service,
+                Err(err) => {
+                    return Err(err.err);
+                }
+            };
+            buf = match ready.call(buf).await {
+                Ok(buf) => buf,
+                Err(err) => {
+                    let report = Report::new(err.err.as_ref()).pretty(true);
+                    tracing::error!("{report:?}");
+                    err.buf
+                }
+            };
+            stream.write_buf(&mut buf).await.map_err(Box::new)?;
         }
     }
 }
