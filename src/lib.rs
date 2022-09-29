080e83716ad5ec1f9fefb78c8584405b6236fbcb
Robert Oleynik <robert.oleynik@proton.me>
2022-09-29 12:45:19 +0200

    Reexport tower crate

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,8 @@ pub mod export {
 }
 
 pub mod service {
+    pub use tower::*;
+
     #[derive(Debug, thiserror::Error)]
     #[error("service `{0}` not ready")]
     pub struct NotReady(pub &'static str);
